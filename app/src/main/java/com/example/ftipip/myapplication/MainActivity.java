135bc460abbc0ae34b9c97ef172b8780206df5e8
Semen Grigorev <ftip@list.ru>
2015-08-24 22:19:33 +0300

    onCreate

--- app/src/main/java/com/example/ftipip/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ftipip/myapplication/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.ftipip.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getCanonicalName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "onCreate");
     }
 
     @Override
